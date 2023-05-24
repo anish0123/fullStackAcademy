8dfbee2199aa4cae8b8c4bccdd5ab0cd85ce3903
anish0123 <anishmhn@hotmail.com>
2023-05-24 13:52:47 +0300

    learning of chapter 2 and exercises till 2.20 done

--- part2/countries/src/components/SingleCountry.js
+++ part2/countries/src/components/SingleCountry.js
@@ -1,20 +1,56 @@
 import Language from "./Language";
+import ApiService from "../services/Api";
+import { useState, useEffect } from "react";
 
-const SingleCountry = ({ country }) => (
-  <div>
-    <h1>{country.name.common}</h1>
-    <p>Capital {country.capital[0]}</p>
-    <p>Area {country.area}</p>
-    <div>
-      <h3>languages: </h3>
-      <ul>
-        {Object.getOwnPropertyNames(country.languages).map((language) => (
-          <Language language={language} key={language} />
-        ))}
-      </ul>
-      <img src={country.flags.png} alt="country flag" />
-    </div>
-  </div>
-);
+const SingleCountry = ({ country }) => {
+  const [weather, setWeather] = useState([]);
+  const [weatherIcon, setWeatherIcon] = useState("");
+  const api_key = process.env.REACT_APP_API_KEY;
+
+  const fetchWeather = () => {
+    ApiService.getWeather(country.latlng[0], country.latlng[1], api_key).then(
+      (weather) => {
+        setWeather([weather]);
+        setWeatherIcon(weather.weather[0].icon);
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  if (weather.length === 0) {
+    console.log(weather)
+    return (<></>)
+  } else {
+    console.log(weather);
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>Capital {country.capital[0]}</p>
+        <p>Area {country.area}</p>
+        <div>
+          <h3>languages: </h3>
+          <ul>
+            {Object.values(country.languages).map((language) => (
+              <Language language={language} key={language} />
+            ))}
+          </ul>
+          <img src={country.flags.png} alt="country flag" />
+        </div>
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <p>temperature {(weather[0].main.temp).toFixed(2)} Celsius</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+            alt="weathericon"
+          />
+          <p> wind {weather[0].wind.speed} m/s</p>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default SingleCountry;
--- part2/countries/src/services/Api.js
+++ part2/countries/src/services/Api.js
@@ -8,12 +8,18 @@ const getCountries = () => {
   return request.then((response) => response.data);
 };
 
-const getCountry = (name ) => {
-
+const getCountry = (name) => {
   const request = axios.get(
     `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
   );
-  return request.then((response) => response.data );
+  return request.then((response) => response.data);
 };
 
-export default { getCountries, getCountry };
+const getWeather = (lat, lon, apiKey) => {
+  const request = axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+  );
+  return request.then((response) => response.data);
+};
+
+export default { getCountries, getCountry, getWeather };
