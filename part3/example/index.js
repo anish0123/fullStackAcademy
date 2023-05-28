47eddc8e58a2d7f6127bb5ca2e33c7123ea92cf8
anish0123 <anishmhn@hotmail.com>
2023-05-28 11:15:28 +0300

    learning and exercises till 3.11 done

--- part3/example/index.js
+++ part3/example/index.js
@@ -1,7 +1,10 @@
 const { response, application } = require("express");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json())
+app.use(cors());
+
 
 let notes = [
   {
