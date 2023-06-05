c3d007656e0ddd09f7f62c1709b8ec89c8bb011a
anish0123 <anishmhn@hotmail.com>
2023-06-05 16:04:28 +0300

    started part 4 and exercises till 4.2 is done

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,31 +1,35 @@
-const Notification = ({message}) => {
-    const error = message.includes("has already been removed");
-    const messageStyle = {
-        color: "green",
-        background: "lightgrey",
-        fontSize: 20,
-        borderStyle: "solid",
-        borderRadius: 5,
-        padding: 10,
-        marginBottom: 10,
-    }
-    const errorStyle = {
-        color: "red",
-        background: "lightgrey",
-        fontSize: 20,
-        borderStyle: "solid",
-        borderRadius: 5,
-        padding: 10,
-        marginBottom: 10,
-    }
- if(message === "") {
-    return (null)
- }
- return(
-    <div style={error ? errorStyle: messageStyle}>
-        {message}
-    </div>
- )
+const Notification = ({ errorMessage, message }) => {
+  console.log("🚀 ~ file: Notification.js:2 ~ Notification ~ message:", message)
+  
+  const error = errorMessage !== undefined && message === "";
+  const messageStyle = {
+    color: "green",
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+  const errorStyle = {
+    color: "red",
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+
+  if (message === "" && errorMessage === "") {
+    return null;
+  } else if(error) {
+    return <div style={errorStyle}>{errorMessage}</div>
+  } else if(!error) {
+    console.log(message)
+    return <div style={messageStyle}>{message}</div>
+  }
+
 };
 
 export default Notification;
