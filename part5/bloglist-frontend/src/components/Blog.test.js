2f9a05e95f899516aeaa53155b5c4ef99661d3a6
anish0123 <anishmhn@hotmail.com>
2023-06-18 11:17:39 +0300

    exercise 5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('renders blog title and author', () => {
   const blog = {
@@ -13,4 +14,25 @@ test('renders blog title and author', () => {
 
   const element = screen.getByText('Blog testing Anish')
   expect(element).toBeDefined()
+})
+
+test('renders blog url and likes when button is clicked', async () => {
+  const blog = {
+    title: 'Blog testing',
+    author: 'Anish',
+    url : 'blogtesting.com',
+    likes: 7
+  }
+  render(<Blog blog={blog}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const url = screen.getByText('blogtesting.com')
+  const likes = screen.getByText('7', { exact : false })
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
+
+
 })
